268ef545b478d3d5903cd01085aa520dad11363c
Javaperez2023 <126530055+Javaperez2023@users.noreply.github.com>
2025-02-22 21:16:04 -0500

    1

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.getElementById('showBtn').addEventListener('click', function() {
-    document.getElementById('messageBox').classList.remove('hidden');
+    setTimeout(function() {
+        document.getElementById('messageBox').classList.remove('hidden');
+    }, 10000); // 10 seconds
 });
 
 document.getElementById('closeBtn').addEventListener('click', function() {
